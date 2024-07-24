4f29ab3d78fb340bbba70a61aea89e0f2c34a5aa
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-07-24 14:45:05 +0200

    .

--- Intrusion Detection System (IDS)/IDS.py
+++ Intrusion Detection System (IDS)/IDS.py
@@ -12,7 +12,7 @@ port_scan_time_window = 60  # Intervalo de tiempo en segundos
 connection_attempts = defaultdict(list)
 
 # Variables para detección de ataques brute force.
-brute_force_threshold = 5  # Número de intentos fallidos de conexión.
+brute_force_threshold = 5  # Número de intentos fallidos de conexión
 brute_force_time_window = 60  # Intervalo de tiempo en segundos
 failed_login_attempts = defaultdict(list)
 
