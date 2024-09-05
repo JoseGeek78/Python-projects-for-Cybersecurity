e41c334e0614fdc5a764af0f9ad7f844d8125265
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-09-05 07:59:21 +0200

    .

--- Honeypot/Honeypot.py
+++ Honeypot/Honeypot.py
@@ -4,7 +4,7 @@ import socket
 import threading
 import logging
 
-# Configuración del logging.jk
+# Configuración del logging.
 logging.basicConfig(filename='honeypot.log', level=logging.INFO, 
                     format='%(asctime)s - %(message)s')
 
