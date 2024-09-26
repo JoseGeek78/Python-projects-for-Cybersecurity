8593f515a0a6b3f0005026a2fda78e256bb853c2
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-09-26 14:59:17 +0200

    ..

--- WiFi/wifi.py
+++ WiFi/wifi.py
@@ -5,3 +5,5 @@ perfil_red = input('Introduce el nombre del perfil de red WiFi: ')
 try:
     
     resultados = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', perfil_red, 'key_clear'], shell=True).decode('utf-80', errors='backslashreplace')
+    
+    
